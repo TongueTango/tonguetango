455340d3ec1eb0cdd70493f8588a3ea2e490c3f0
Dmitri <Popov>
2012-09-28 12:47:45 +0400

    updating to new version

--- dev/api/protected/extensions/notification/classes/notification.php
+++
@@ -1,6 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-abstract class Notification extends Kohana_Notification
-{
-	
-}
--- dev/api/protected/extensions/notification/classes/notification/adapter.php
+++
@@ -1,6 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-abstract class Notification_Adapter extends Kohana_Notification_Adapter
-{
-	
-}
